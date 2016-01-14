68fc8d4288a6bb778e431a5157e67d16e09dc83d
Tim Bart <tim@sayhello.com>
2016-01-14 15:09:29 -0800

    Moving uptime query to redshift

--- src/main/java/com/hello/suripu/admin/resources/v1/DiagnosticResources.java
+++ src/main/java/com/hello/suripu/admin/resources/v1/DiagnosticResources.java
@@ -1,21 +1,28 @@
 package com.hello.suripu.admin.resources.v1;
 
+import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
+import com.google.common.primitives.Longs;
 import com.hello.suripu.admin.Util;
-import com.hello.suripu.coredw8.oauth.AccessToken;
-import com.hello.suripu.coredw8.oauth.Auth;
-import com.hello.suripu.coredw8.oauth.ScopesAllowed;
+import com.hello.suripu.admin.db.UptimeDAO;
 import com.hello.suripu.core.db.AccountDAO;
 import com.hello.suripu.core.db.DeviceDAO;
 import com.hello.suripu.core.diagnostic.Count;
-import com.hello.suripu.core.diagnostic.DiagnosticDAO;
 import com.hello.suripu.core.models.Account;
 import com.hello.suripu.core.models.DeviceAccountPair;
 import com.hello.suripu.core.oauth.OAuthScope;
 import com.hello.suripu.core.tracking.Category;
 import com.hello.suripu.core.tracking.TrackingDAO;
 import com.hello.suripu.core.util.JsonError;
-import com.codahale.metrics.annotation.Timed;
+import com.hello.suripu.coredw8.oauth.AccessToken;
+import com.hello.suripu.coredw8.oauth.Auth;
+import com.hello.suripu.coredw8.oauth.ScopesAllowed;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Hours;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -27,24 +34,30 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Map;
 
 @Path("/v1/diagnostic")
 public class DiagnosticResources {
 
-    private final DiagnosticDAO diagnosticDAO;
     private final AccountDAO accountDAO;
     private final DeviceDAO deviceDAO;
     private final TrackingDAO trackingDAO;
+    private final UptimeDAO uptimeDAO;
 
+    private final static Ordering<Count> byMillisOrdering = new Ordering<Count>() {
+        public int compare(Count left, Count right) {
+            return Longs.compare(left.date.getMillis(), right.date.getMillis());
+        }
+    };
 
-    public DiagnosticResources(final DiagnosticDAO diagnosticDAO,
-                               final AccountDAO accountDAO,
+    public DiagnosticResources(final AccountDAO accountDAO,
                                final DeviceDAO deviceDAO,
-                               final TrackingDAO trackingDAO) {
-        this.diagnosticDAO = diagnosticDAO;
+                               final TrackingDAO trackingDAO,
+                               final UptimeDAO uptimeDAO) {
         this.accountDAO = accountDAO;
         this.deviceDAO = deviceDAO;
         this.trackingDAO = trackingDAO;
+        this.uptimeDAO = uptimeDAO;
     }
 
     @ScopesAllowed({OAuthScope.ADMINISTRATION_READ})
@@ -54,7 +67,7 @@ public class DiagnosticResources {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Count> uptime(@Auth final AccessToken accessToken,
                               @PathParam("email") final String email,
-                              @DefaultValue("false") @QueryParam("padded") Boolean padded) {
+                              @DefaultValue("true") @QueryParam("padded") Boolean padded) {
 
         final Optional<Long> accountIdOptional = Util.getAccountIdByEmail(accountDAO, email);
         if(!accountIdOptional.isPresent()) {
@@ -68,12 +81,22 @@ public class DiagnosticResources {
                     new JsonError(Response.Status.NOT_FOUND.getStatusCode(), "Device not found")).build());
         }
 
-        if(padded) {
-            final List<Count> counts = diagnosticDAO.uptimePadded(accountIdOptional.get(), deviceAccountPairOptional.get().internalDeviceId);
-            return counts;
+        final DateTime start = DateTime.now(DateTimeZone.UTC).withMinuteOfHour(0).withSecondOfMinute(0).withMillisOfSecond(0);
+
+        final Hours hours = Hours.hoursBetween(start, DateTime.now(DateTimeZone.UTC).minusDays(10));
+        final Map<Long, Count> pad = Maps.newHashMap();
+        for(int i =1; i < hours.getHours(); i++) {
+            final DateTime dt = start.minusHours(i);
+            pad.put(dt.getMillis(), new Count(dt, 0));
         }
 
-        return diagnosticDAO.uptime(accountIdOptional.get(), deviceAccountPairOptional.get().internalDeviceId);
+        final List<Count> counts = uptimeDAO.uptime(accountIdOptional.get());
+        for(final Count c : counts) {
+            pad.put(c.date.getMillis(), c);
+        }
+
+        return byMillisOrdering.sortedCopy(pad.values());
+
     }
 
     @ScopesAllowed({OAuthScope.ADMINISTRATION_WRITE})
